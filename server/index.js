f09eaaee1e014aee255469b686183444e7d7dcdb
HTMLAPPLICATION <amineabida@gmail.com>
2026-05-25 11:15:06 +0100

    Fix server with health route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.get("/health", (req, res) => res.json({status:"ok"}));
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
-app.listen(PORT, () => console.log("Running on port " + PORT));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.listen(PORT, () => console.log("Cabinet Juridique Maroc - port " + PORT));
