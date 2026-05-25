6173eb9e8152665fd7be42f165ec073bfc72eb8e
HTMLAPPLICATION <amineabida@gmail.com>
2026-05-25 10:53:10 +0100

    Fix server no helmet

--- server/index.js
+++ server/index.js
@@ -1,21 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
-
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
+app.get("/health", (req, res) => res.json({ status: "ok" }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log("Cabinet Juridique Maroc port " + PORT);
-});
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+app.listen(PORT, () => console.log("Cabinet Juridique Maroc port " + PORT));
